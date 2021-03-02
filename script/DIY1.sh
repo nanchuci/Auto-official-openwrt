577434f31ed9a52ca51c2be235c8211df6fd2c4e
nanchuci <71806013+nanchuci@users.noreply.github.com>
2021-03-02 22:41:26 +0800

    Update DIY1.sh

--- script/DIY1.sh
+++ script/DIY1.sh
@@ -6,7 +6,7 @@
 sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
 
 # 修改主机名字，把OpenWrt-123修改你喜欢的就行（不能纯数字或者使用中文）
-sed -i 's/ImmortalWrt/E8820S/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/E8820S/g' package/base-files/files/bin/config_generate
 
 # 修改开源驱动wifi名称
 sed -i 's/OpenWrt/ZTE-E8820S-%s/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
