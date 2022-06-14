9b68959c6861ddd535f93444f1d1c6150bac8ad4
nanchuci <71806013+nanchuci@users.noreply.github.com>
2022-06-14 21:36:18 +0800

    update

--- script/diy-part1.sh
+++ script/diy-part1.sh
@@ -18,11 +18,11 @@
 #sed -i '$a src-git diy https://github.com/CCnut/feed-netkeeper.git;LUCI-LUA-UCITRACK' feeds.conf.default
 
 # Add a feed source
-echo 'src-git jerryk https://github.com/jerrykuku/openwrt-package' >>feeds.conf.default
+echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 # Add luci-theme-argon
 #cd lede/package/lean
 #rm -rf luci-theme-argon 
-#git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-theme-argon
 #git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config
